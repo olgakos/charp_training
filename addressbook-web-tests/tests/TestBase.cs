9972903a45e5c1bb231827781284eff7739eb81b
Olga <49266934+olgakos@users.noreply.github.com>
2020-12-30 13:03:26 +0300

    L 3_2 (Промежуточный) Создана глобальаня переемнная для запуска в одном браузере 2х тестов ГКТ

--- addressbook-web-tests/tests/TestBase.cs
+++ addressbook-web-tests/tests/TestBase.cs
@@ -10,37 +10,22 @@ namespace WebAddressbookTests
     public class TestBase //BN! ТБ публичный класс, чтобы видели другие 2_1
     {
         //ссылка на место хранения ссылок на помощников вместо них самих
+       
         protected ApplicationManager app;
 
         [SetUp]
         public void SetupTest()
         {
-            
-            app = new ApplicationManager(); //инициализируем ссылку на АМ
-            app.Navigator.GoToHomePage(); // на базовый URL
-            app.Auth.Login(new AccountData("admin", "secret"));
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            app.Stop();
+            app = TestSuiteFixture.app; //3_2
+            //3_2 код задогина переехал в общую инициализацию ТСФ
         }
 
 
 
-
-
-        
-
+       // [TearDown]уехало в ТСФ 3_2
 
 
 
-
-
-
-
-        
 
     }
 }
---
+++ addressbook-web-tests/tests/TestSuiteFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; //rконструкция импортирует классы
+
+namespace WebAddressbookTests
+{
+
+    [SetUpFixture]
+    public class TestSuiteFixture
+    {
+
+        public static ApplicationManager app; //поле АМ (из IAM)
+
+        [SetUp]
+        public void InitApplicationManager()
+        {
+            app = new ApplicationManager(); //создали АМ
+            app.Navigator.GoToHomePage(); // на базовый URL
+            app.Auth.Login(new AccountData("admin", "secret"));
+        }
+
+
+
+        [TearDown]
+        public void StopApplicationManager()
+        {
+            app.Stop(); //браузер остановистя 1 раз после выполения ВСЕХ тестов
+        }
+
+
+    }
+}
