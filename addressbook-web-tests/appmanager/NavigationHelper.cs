05622cf2216ef0c6dee876d2893186ceb41d4081
Olga <49266934+olgakos@users.noreply.github.com>
2020-12-30 16:02:21 +0300

    HW 2_08  Задание №8 повысить "интеллектуальность" тестов

--- addressbook-web-tests/appmanager/NavigationHelper.cs
+++ addressbook-web-tests/appmanager/NavigationHelper.cs
@@ -26,7 +26,13 @@ namespace WebAddressbookTests
         public void GoToHomePage() //стартовая страница приложения
         {
             //OpenHomePage
+
+            if (driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL);
+            //driver.Navigate().GoToUrl(baseURL + "/addressbook/"); не поняла L3_5 03.55
         }
 
 
@@ -34,20 +40,33 @@ namespace WebAddressbookTests
 
         public void GoToGroupsPage()
         {
+            //L3_4 «Если на странице уже есть адрес такой-то + есть кнопка «НьюГрупп», то делать ничего не надо. ИНАЧЕ, то надо нажать и перейти»
+
+            if (driver.Url == baseURL + "/addressbook/group.php"
+                && IsElementPresent(By.Name("new")))
+            {
+                return; 
+            }
+
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
 
 
 
-        public void GoToHome() //Точно попасть на страницу "Контакты"
+        public void GoToHome() 
         {
+            //Точно ли попали на страницу "Контакты"?
+            //Проверка, что ссли мы уже там...
+            if (driver.Url == baseURL + "/addressbook/"
+                //&& IsElementPresent(By.title(""Sort on “First name""))
+                && IsElementPresent(By.LinkText("First name"))
+                )
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("home")).Click();
         }
-
-
-
-
 
 
     }
