619efbc5abc778e26e014a14ba42e9590cef8612
Olga <ok2304@yandex.ru>
2021-01-26 11:11:16 +0300

    m6l1 (промежуточный) успешно сгенерирован и скопирован в папку charp_training\addressbook-web-tests файл groups.csv с 2 записями)

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAddressbookTests; //m6l1
+using System.IO; //m6l1
 
 namespace addressbook_test_data_generators
 {
@@ -10,7 +12,18 @@ namespace addressbook_test_data_generators
     {
         static void Main(string[] args)
         {
-            System.Console.Out.Write("Hello world!");
+            //System.Console.Out.Write("Hello world!");
+            int count = Convert.ToInt32(args[0]);
+            StreamWriter writer = new StreamWriter(args[1]);
+            for (int i = 0; i < count; i++)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10),
+                    TestBase.GenerateRandomString(10)));
+            }
+
+            writer.Close();
         }
     }
 }
