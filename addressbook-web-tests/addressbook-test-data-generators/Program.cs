03c6f7a5020afa6b86aee7fd581c0639cff3e07f
Olga <49266934+olgakos@users.noreply.github.com>
2021-01-28 22:20:24 +0300

    Revert "m6 l4 fin (промежуточный) добавлен файл Эксель. Но проблема с запуском ГКТ"
    This reverts commit 0367b9bd27976acd58bb8ac211ab55322ea2bf41.

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -9,8 +9,6 @@ using System.Xml; //m6 l2
 using System.Xml.Serialization; //m6 l2
 using Newtonsoft.Json; //m6 l3
 using Formatting = Newtonsoft.Json.Formatting; //m6 l3
-using Excel = Microsoft.Office.Interop.Excel; //6_4
-
 
 namespace addressbook_test_data_generators
 {
@@ -20,8 +18,9 @@ namespace addressbook_test_data_generators
         {
             //System.Console.Out.Write("Hello world!");
             int count = Convert.ToInt32(args[0]);
-            //StreamWriter writer = new StreamWriter(args[1]); //6_4
-            string filename = args[1]; //6_4
+            StreamWriter writer = new StreamWriter(args[1]);
+
+
             //string format = args[3];
             string format = args[2];
 
@@ -37,26 +36,25 @@ namespace addressbook_test_data_generators
             }
 
 
-
-
-
-
-            if (format == "excel")
-            { writeGroupsToExcelFile(groups, filename); }
-
+            if (format == "csv")
+            {
+                writeGroupsToCsvFile(groups, writer);
+            }
+            else if (format == "xml")
+            {
+                writeGroupsToXmlFile(groups, writer);
+            }
+            else if (format == "json")
+            {
+                writeGroupsToJsonFile(groups, writer);
+            }
             else
             {
-                StreamWriter writer = new StreamWriter(args[1]);
-                if (format == "csv")
-                { writeGroupsToCsvFile(groups, writer); }
-                else if (format == "xml")
-                { writeGroupsToXmlFile(groups, writer); }
-                else if (format == "json")
-                { writeGroupsToJsonFile(groups, writer); }
-                else
-                { System.Console.Out.Write("Unrecognized format " + format); }
-                writer.Close();
+                System.Console.Out.Write("Unrecognized format " + format);
             }
+
+
+            writer.Close();
         }
 
 
@@ -74,43 +72,17 @@ namespace addressbook_test_data_generators
 
 
         static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
-        { new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups); }
-
-
-
-        //Лекция 6.3. Чтение и запись файлов в формате Json
-        static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer) //6_3
-        { writer.Write(JsonConvert.SerializeObject(groups, Formatting.Indented)); }
-
-
-        //Лекция 6.4. Чтение и запись файлов в формате Excel
-        static void writeGroupsToExcelFile(List<GroupData> groups, string filename)
         {
-            Excel.Application app = new Excel.Application();
-            app.Visible = true;
-            Excel.Workbook wb = app.Workbooks.Add();
-            Excel.Worksheet sheet = wb.ActiveSheet;
-
-            int row = 1;
-
-            foreach (GroupData group in groups) // 6_4 t8-43
-            {
-                sheet.Cells[row, 1] = group.Name;
-                sheet.Cells[row, 2] = group.Header;
-                sheet.Cells[row, 3] = group.Footer;
-                row++;
-            }
-
-            //m6_l4 t11-40
-            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), filename);
-            File.Delete(fullPath);
-            wb.SaveAs(fullPath);
-
-            wb.Close();
-            app.Visible = false;
-            app.Quit();
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
         }
 
+
+
+
+        static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer) //6_3
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Formatting.Indented));
+        }
 
 
 
