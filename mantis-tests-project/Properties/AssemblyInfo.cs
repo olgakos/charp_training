db96751d3153c40af1909a1aa339931fd188d3ec
Olga <ok2304@yandex.ru>
2021-02-11 17:18:26 +0300

    HW 20 (промежуточный) созданы заготовки дял проекта

---
+++ mantis-tests-project/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mantis-tests-project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("mantis-tests-project")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("518d9509-0583-44db-8fd9-6446f8e9bf2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mantis-tests-project/appmanager/ApplicationManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace mantis_tests_project
+{
+    public class ApplicationManager
+    {
+        protected IWebDriver driver;
+        protected string baseURL;
+
+        //public RegistrationHelper Registration { get; set; }
+        //public FtpHelper Ftp { get; set; }
+        //новые методы сюда
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/mantisbt-2.24.2/login_page.php";
+            //Registration = new RegistrationHelper(this);
+            //Ftp = new FtpHelper(this);
+            //новые методы сюда
+        }
+
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-2.24.2/login_page.php";
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+
+        public IWebDriver Driver
+        {
+            get { return driver; }
+        }
+    }
+}
